34285b49bae21f6a4a5c15586b137fc2a6e13eb8
eder <ederoliveirabarreto@gmail.com>
2021-11-09 09:49:02 -0300

    Add message setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_121855) do
+ActiveRecord::Schema.define(version: 2021_11_09_124406) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
