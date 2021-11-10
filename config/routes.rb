844911b56e5630d32cb2bedd3f9e6e74da125e8e
eder <ederoliveirabarreto@gmail.com>
2021-11-10 16:13:18 -0300

    Add messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, only: [:new, :create]
+  resources :messages, only: [:create]
+  # resources :users, only: [:new, :create]
 end
