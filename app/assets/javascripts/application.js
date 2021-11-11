a4816b6fad1ca547826d16eba7bf0b5afab10eee
eder <ederoliveirabarreto@gmail.com>
2021-11-11 09:26:56 -0300

    Created a custom scope and improved message input functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,10 +23,22 @@ scroll_bottom = function () {
   }
 };
 
+submit_message = function () {
+  $("#message_body").on("keydown", function (e) {
+    if (e.keyCode === 13) {
+      if (e.target.value) {
+        $("button").click();
+        e.target.value = "";
+      }
+    }
+  });
+};
+
 $(document).on("turbolinks:load", function () {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function () {
     $(this).closest(".message").transition("fade");
   });
+  submit_message();
   scroll_bottom();
 });
